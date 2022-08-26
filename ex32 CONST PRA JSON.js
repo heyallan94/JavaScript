2a318f4b0576ca3fe961d2b83c438cd79eaa0940
Allan <heyallan16@yahoo.com.br>
2022-08-26 14:26:17 -0300

    Aprendizado sobre Fetch API, json e tratamento de dados

--- ex32 CONST PRA JSON.js
+++ ex32 CONST PRA JSON.js
@@ -15,9 +15,22 @@ const vingadores = [{
     sobrenome : 'Banner',
     fortuna : '14.000,00 U$D',
     codinome : 'Hulk',
-    esposa : null
-}]
+    esposa : null,
+},]
 
-let registro = vingadores.map(a => a.nome)
-console.log(registro)
+let registro = vingadores.map(qualquernome => qualquernome.nome) //Note que aqui ele criou a variável let e o map 
+console.log(registro)                                            //fez a nova lista com as propriedades que pediu!
 
+//Com o objeto FEITO, Deve-se transformar em JSON. Mas lembre-se que JSON aceita apenas texto
+//então TUDO TUDO deve está dentro e "" e no final não pode finalizar com virgulas como é o caso de objetos que 
+//podemos
+
+console.log(typeof vingadores) //Aqui é um objeto
+
+const jsonvingadores = JSON.stringify(vingadores) //Transformando em JSON
+
+console.log(typeof jsonvingadores)//Note que agora é uma string
+
+const novoVingadores = JSON.parse(jsonvingadores) //Transformando em OBJETO
+
+console.log(typeof novoVingadores)//Agora o JSON virou um objeto do Javascript
