cc5252044ced314aa9bc1be3b945841112d70370
Allan <heyallan16@yahoo.com.br>
2023-03-02 21:07:41 -0300

    Aprendizado 2023 mar

--- ex24 objetos.js
+++ ex24 objetos.js
@@ -8,11 +8,17 @@ let pessoa = { //inicia com chaves
     nome: "Caio", //ao digitar pessoa.nome, vai aparecer o dado "Caio"
     idade: 23, //Adcionando number
     cidade: n, //adicionando variável
-    peso: "80kg" //adicionando string
+    peso: "80kg", //adicionando string
+    exercicio : function(){
+                    for(let i = 0; i < 5 ; i++){
+                         console.log(i)
+                        }
+                }
 }
 
 console.log(pessoa) //impromir todo o objeto
 console.log(pessoa.nome) //imprimir um dado do objeto
 console.log(pessoa.casa)// não existe um dado chamado casa no objeto, então dá undefined
-console.log(pessoa.cidade)
+console.log(pessoa.cidade) // variável
+console.log(pessoa.exercicio()) //Note que é assim que chamamos função dentro de objeto
 //objetos são separados por vírgula, não esquecer
